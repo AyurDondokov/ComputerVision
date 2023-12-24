49181475b920bd52f4d282952edefe4836a48f3a
AyurDondokov <ayur00dondokov@gmail.com>
2023-12-25 06:42:10 +0800

    ExamTask6

--- Exam/Task4/main.py
+++ Exam/Task4/main.py
@@ -3,7 +3,11 @@ import numpy as np
 from skimage import filters, measure
 
 
-def find_circle(imr):
+def find_circle(img_path):
+    image = cv2.imread(img_path, 0)
+    binary_image = (image < filters.threshold_otsu(image)).astype(np.uint8)
+    _, labeled_regions = cv2.connectedComponents(binary_image)
+    imr = measure.regionprops(labeled_regions)
     max_area = imr[0].area
     result = [0, imr[0].axis_major_length, imr[0].axis_minor_length]
     for i in range(1, len(imr)):
@@ -13,17 +17,8 @@ def find_circle(imr):
     return result
 
 
-def circle(img_path):
-    image = cv2.imread(img_path, 0)
-    binary_image = (image < filters.threshold_otsu(image)).astype(np.uint8)
-    _, labeled_regions = cv2.connectedComponents(binary_image)
-    result = measure.regionprops(labeled_regions)
-    return result
-
-
-image_path = "task4.jpg"
-mr = circle(image_path)
-arr = find_circle(mr)
+path = "task4.jpg"
+arr = find_circle(path)
 nominal_resolution = 1.05714
 length = (arr[1] + arr[2]) / 2 * nominal_resolution
 print(f"The real diameter of the ball: {length}")
