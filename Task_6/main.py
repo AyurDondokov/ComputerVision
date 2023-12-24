f3978e6dcdcfcd6312f0c80007beb1770fdfc875
AyurDondokov <ayur00dondokov@gmail.com>
2023-12-25 05:57:33 +0800

    Task6

--- Task_6/main.py
+++ Task_6/main.py
@@ -4,7 +4,7 @@ from skimage.measure import label, regionprops
 
 
 def filling_factor(reg):
-    return reg.img.mean()
+    return reg.image.mean()
 
 
 def recognize(reg):
@@ -13,13 +13,13 @@ def recognize(reg):
     else:
         euler = reg.euler_number
         if euler == -1:
-            if 1 in reg.img.mean(0)[:2]:
+            if 1 in reg.image.mean(0)[:2]:
                 return "B"
             else:
                 return "8"
         elif euler == 0:
-            tmp = reg.img.copy()
-            if 1 in reg.img.mean(0)[:2]:
+            tmp = reg.image.copy()
+            if 1 in reg.image.mean(0)[:2]:
                 tmp[len(tmp[:, 0]) // 2, :] = 1
                 tmp_labeled = label(tmp)
                 tmp_regions = regionprops(tmp_labeled)
@@ -30,15 +30,15 @@ def recognize(reg):
             tmp[-1, :] = 1
             tmp_labeled = label(tmp)
             tmp_regions = regionprops(tmp_labeled)
-            if 1 in reg.img.mean(0) and reg.eccentricity < 0.5:
+            if 1 in reg.image.mean(0) and reg.eccentricity < 0.5:
                 return '*'
-            if tmp_regions[0].euler_number == -1:
+            elif tmp_regions[0].euler_number == -1:
                 return "A"
             return "0"
         else:
-            if 1 in reg.img.mean(0):
+            if 1 in reg.image.mean(0):
                 return "1"
-            tmp = reg.img.copy()
+            tmp = reg.image.copy()
             tmp[[0, -1], :] = 1
             tmp_labeled = label(tmp)
             tmp_regions = regionprops(tmp_labeled)
@@ -58,7 +58,7 @@ binary = img.mean(2)
 binary[binary != 0] = 1
 
 labeled = label(binary)
-print(np.max(labeled))
+print(f"Number of symbols: {np.max(labeled)}")
 
 regions = regionprops(labeled)
 
@@ -70,10 +70,6 @@ for region in regions:
         counts[symbol] = 0
     counts[symbol] += 1
 
-print(counts)
-# print((labeled.max())/labeled.max())
-print(f'{(labeled.max() - counts.get("?", 0)) / labeled.max()}')
-# plt.imshow(img)
-# plt.imshow(regions[4].image)
-# plt.imshow(regions[7].euler_number)
+print(f"All symbols:\n{counts}")
+print(f'Percentage of character recognition: {(labeled.max() - counts.get("?", 0)) / labeled.max()}')
 plt.show()
